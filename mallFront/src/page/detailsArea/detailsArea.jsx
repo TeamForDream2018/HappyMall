ff01e80bf4603f4a24914f6a36e0390e34c50757
kingsIt <2829238209@qq.com>
2018-03-19 00:17:38 +0800

    "提交"

--- mallFront/src/page/detailsArea/detailsArea.jsx
+++ mallFront/src/page/detailsArea/detailsArea.jsx
@@ -8,10 +8,15 @@ class DetailsArea extends React.Component{
         const {router,details} = this.props;
         const {listdetail} = details;
         return(
-            <div>
+            <div className={styles.main_cont}>
                 {
                     listdetail && listdetail.children && listdetail.children.map((item,index)=>{
-                        return <span key={"details_"+index}>{item.name}</span>
+                        return <div className={styles.picOne} key={"picOne"+index}>
+                            <img src={item.imgurl} className={styles.imgclass}/>
+                            <p className={styles.intro}>{item.name}</p>
+                            <div className={styles.price_cont}><p style={{marginRight:"5rem"}}><span>原价：</span><span className={styles.discont_price}>￥39.3</span></p>
+                                                                <p><span className={styles.current_price_info}>现价：</span><span className={styles.current_price}>￥33.3</span></p></div>
+                        </div>
                     })
                 }
             </div>
